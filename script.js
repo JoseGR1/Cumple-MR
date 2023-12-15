f6c3c3f8cafc4da561c3e1d51e81236520d97ff2
JoseGR1 <120201294+JoseGR1@users.noreply.github.com>
2023-12-14 22:35:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  startConfetti();
+});
+
+function startConfetti() {
+  particlesJS("confetti-container", {
+      particles: {
+          number: {
+              value: 150,  // Puedes ajustar este valor para tener más confeti
+          },
+          color: {
+              value: ["#3498db", "#2ecc71", "#e67e22", "#9b59b6", "#f39c12", "#1abc9c"],
+          },
+          shape: {
+              type: "circle",
+          },
+          opacity: {
+              value: 0.8,
+          },
+          size: {
+              value: 6,
+          },
+          move: {
+              direction: "random",
+              speed: 3,
+          },
+      },
+  });
+}
